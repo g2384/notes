4b575abbb5a50c435165dccf013ab7b49dd9585f
g2384 <g2384@users.noreply.github.com>
2023-05-21 22:03:29 +0100

    update

---
+++ docs/cs_examples/cs_examples/DeadLockMonitor.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Threading;
+
+namespace cs_examples
+{
+    internal class DeadLockMonitor
+    {
+        private static object lock1 = new object();
+        private static object lock2 = new object();
+
+        private static void AcquireLocks1()
+        {
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+
+            while (true)
+            {
+                if (Monitor.TryEnter(lock1, TimeSpan.FromSeconds(1)))
+                {
+                    try
+                    {
+                        Console.WriteLine($"Thread {threadId} acquired lock 1.");
+                        Thread.Sleep(1000);
+                        Console.WriteLine($"Thread {threadId} attempted to acquire lock 2.");
+
+                        if (Monitor.TryEnter(lock2, TimeSpan.FromSeconds(1)))
+                        {
+                            try
+                            {
+                                Console.WriteLine($"Thread {threadId} acquired lock 2.");
+                                break; // exit the loop if both locks are acquired
+                            }
+                            finally
+                            {
+                                Monitor.Exit(lock2);
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        Monitor.Exit(lock1);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Thread {threadId} is done.");
+        }
+
+        private static void AcquireLocks2()
+        {
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+
+            while (true)
+            {
+                if (Monitor.TryEnter(lock2, TimeSpan.FromSeconds(1)))
+                {
+                    try
+                    {
+                        Console.WriteLine($"Thread {threadId} acquired lock 2.");
+                        Thread.Sleep(1000);
+                        Console.WriteLine($"Thread {threadId} attempted to acquire lock 1.");
+
+                        if (Monitor.TryEnter(lock1, TimeSpan.FromSeconds(1)))
+                        {
+                            try
+                            {
+                                Console.WriteLine($"Thread {threadId} acquired lock 1.");
+                                break; // exit the loop if both locks are acquired
+                            }
+                            finally
+                            {
+                                Monitor.Exit(lock1);
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        Monitor.Exit(lock2);
+                    }
+                }
+            }
+
+            Console.WriteLine($"Thread {threadId} is done.");
+        }
+
+        public static void Start()
+        {
+            // Create two new threads that compete for the locks
+            var thread1 = new Thread(AcquireLocks1);
+            var thread2 = new Thread(AcquireLocks2);
+
+            // Start the threads
+            thread1.Start();
+            thread2.Start();
+
+            // Wait for the threads to complete
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine("Finished.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ docs/cs_examples/cs_examples/StackSorting.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cs_examples
+{
+    public static class StackSorting
+    {
+        public static void Start()
+        {
+            Stack<int> stack = new Stack<int>();
+
+            // Push elements into the stack
+            stack.Push(5);
+            stack.Push(3);
+            stack.Push(8);
+            stack.Push(1);
+            stack.Push(2);
+
+            Console.WriteLine("Original Stack:");
+            PrintStack(stack);
+
+            // Sort the stack in ascending order
+            Stack<int> sortedStack = SortStack(stack);
+
+            Console.WriteLine("Sorted Stack:");
+            PrintStack(sortedStack);
+        }
+
+        static Stack<int> SortStack(Stack<int> stack)
+        {
+            Stack<int> sortedStack = new Stack<int>();
+
+            while (stack.Count > 0)
+            {
+                int temp = stack.Pop();
+
+                // Move elements from sortedStack to stack
+                while (sortedStack.Count > 0 && sortedStack.Peek() > temp)
+                {
+                    stack.Push(sortedStack.Pop());
+                }
+
+                // Push temp in sorted order to sortedStack
+                sortedStack.Push(temp);
+            }
+
+            return sortedStack;
+        }
+
+        static void PrintStack(Stack<int> stack)
+        {
+            foreach (int element in stack)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
